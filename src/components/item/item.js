e20b06b958c0178d92538504f08279acf7867e68
koodaajakige <kirsi.nykanen@edu.sasky.fi>
2022-04-15 19:40:54 +0300

    Items and item components added

---
+++ src/components/item/item.js
@@ -0,0 +1,18 @@
+import styles from './item.module.scss';
+
+function Item(props) {
+    return (
+        <div className={styles.item}>
+            <div className={styles.item_data}>
+                <div className={styles.item_type}>Sähkö</div>
+                <div className={styles.item_amount}>437,50 €</div>
+                <div className={styles.item_date}>20.3.2019</div>
+                <div className={styles.item_timespan}>1.12.2018-28.2.2019</div>
+                <div className={styles.item_receiver}>Fortum</div>
+                <div className={styles.item_average}>145,83 €/kk</div>
+            </div>
+        </div>
+    );
+}
+
+export default Item;

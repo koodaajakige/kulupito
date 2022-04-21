c6f5aa2f804de378657de7ec0aa227ab52279936
koodaajakige <kirsi.nykanen@edu.sasky.fi>
2022-04-21 12:23:28 +0300

    React router added

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,7 +1,10 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import styles from './app.module.scss';
 import Header from '../header';
 import Content from '../content';
-import Items from '../items';
+import Items from '../../routes/items';
+import Stats from '../../routes/stats';
+import Settings from '../../routes/settings';
 import Menu from '../menu';
 import { ButtonAppContainer} from '../../shared/uibuttons';
 
@@ -9,11 +12,21 @@ function App() {
   return (
     <ButtonAppContainer>
       <div className={styles.app}>
-        <Header />
-        <Content>
-          <Items />
-        </Content>
-        <Menu />
+        <Router>
+          <Header />
+          <Content>
+            <Route exact path="/">
+              <Items />
+            </Route>
+            <Route path="/stats">
+              <Stats />
+            </Route>
+            <Route path="/settings">
+              <Settings />
+            </Route>
+          </Content>
+          <Menu />
+        </Router>
       </div>
     </ButtonAppContainer>
   );
